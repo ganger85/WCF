8d9a56573e9588a440e75877bfc6fd6f061baaba
arincon <arincon@homeaway.com>
2019-02-23 16:53:17 +0100

    CI

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,5 +1,5 @@
 module.exports.init = () => {
-    process.env.mastersTable = "masters-devmanolete";
+    process.env.mastersTable = `masters-${process.env.TEST_STAGE}`;
     process.env.AWS_REGION = "eu-west-1";
   };
   
